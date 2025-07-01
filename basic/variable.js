d4634fb59bd81c61d5a87bed9ba1eff7d1b63cfe
Mousam <mousammishra79@gmail.com>
2025-07-01 15:18:38 +0000

    variable

---
+++ basic/variable.js
@@ -0,0 +1,12 @@
+const accountId =14453;
+let accountEmail ="mousam@gmail.com";
+var accountPassword="1234";
+accountCity="Delhi";
+// accountId=2; not allow
+accountEmail="m@.com";
+accountPassword="23";
+accountCity="Patna";
+console.table([accountId,accountEmail,accountPassword,accountCity]);
+
+/*do not use var
+becauseof issue in block scope and functional scope */
